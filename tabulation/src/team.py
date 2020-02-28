338e3d13579ace6916fb80efead72e939d758102
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2020-02-28 14:31:33 -0500

    changed to match make criteria

--- tabulation/src/team.py
+++ tabulation/src/team.py
@@ -2,7 +2,7 @@ class Team:
     def __init__(self, name, scores):
         self.team_name = name
         self.tech = scores[0]
-        self.intu = scores[1]
+        self.pol = scores[1]
         self.imp = scores[2]
         self.cre = scores[3]
         self.wow = scores[4]
