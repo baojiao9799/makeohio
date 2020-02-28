a9f671b1628bbd7bcc70185647a338abd612f4c5
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2020-02-28 14:14:59 -0500

    updated company sponsor challenge names

--- team-separation/src/companies.py
+++ team-separation/src/companies.py
@@ -1,8 +1,3 @@
 # Name in export sheet
 AEP = 'AEP'
-CROWN = 'Crown'
-ENGIE = 'ENGIE'
-HONDA = 'Honda/Mobikit'
-JPMC = 'JP Morgan & Chase'
-MICROSOFT = 'Microsoft'
-STATE_AUTO = 'State Auto'
+HONDA = 'Honda'
