e6a7df74935ecb5f13ee6eff9853826aeb740fd0
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2020-02-29 11:58:07 -0500

    edited to match survey

--- tabulation/src/team.py
+++ tabulation/src/team.py
@@ -2,9 +2,9 @@ class Team:
     def __init__(self, name, scores, in_house):
         self.team_name = name
         self.tech = scores[0]
-        self.pol = scores[1]
+        self.cre = scores[1]
         self.imp = scores[2]
-        self.cre = scores[3]
+        self.pol = scores[3]
         self.wow = scores[4]
         self.in_house = in_house
         sum = 0
