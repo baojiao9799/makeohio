05acf02d7e3cd42ba195cb72043f401749f57482
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2020-02-28 19:08:53 -0500

    updated to match survey

--- team-separation/src/team.py
+++ team-separation/src/team.py
@@ -1,15 +1,22 @@
 class Team:
     def __init__(self, data):
         self.team_name = data[0]
-        self.member_names = data[1]
-        self.member_emails = data[2]
-        self.new = data[3]
-        self.project_name = data[4]
-        self.project_description = data[5]
-        self.hardware = data[6]
-        self.challenges = data[7]
-        self.room = data[8]
-        self.table = data[9]
+        self.member_names = []
+        j = 2
+        for i in range(int(data[1]):
+            self.member_names.append(data[j])
+            j += 2
+        self.member_emails = []
+        j = 3
+        for i in range(int(data[1]):
+            self.member_emails.append(data[j])
+            j += 2
+        self.project_name = data[10]
+        self.project_description = data[11]
+        self.room = data[12]
+        self.table = table[13]
+        self.challenges = data[14]
+        self.github = data[15]
 
         self.judge_pair_1 = None
         self.judge_pair_2 = None
