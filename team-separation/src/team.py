f283c4e1bfe87adbb2e52fc4df23a524264bf659
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2020-02-29 12:39:21 -0500

    changed names to string

--- team-separation/src/team.py
+++ team-separation/src/team.py
@@ -1,12 +1,12 @@
 class Team:
     def __init__(self, data):
         self.team_name = data[0]
-        self.member_names = []
-        j = 2
+        self.member_names = data[2]
+        j = 4
         for i in range(int(data[1])):
-            self.member_names.append(data[j])
+            self.member_names += ", " + data[j]
             j += 2
-        self.member_emails = []
+        self.member_emails = ""
         j = 3
         for i in range(int(data[1])):
             self.member_emails.append(data[j])
@@ -14,7 +14,7 @@ class Team:
         self.project_name = data[10]
         self.project_description = data[11]
         self.room = data[12]
-        self.table = table[13]
+        self.table = data[13]
         self.challenges = data[14]
         self.github = data[15]
 
